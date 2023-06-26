6032d0983075c1f41b0a04c7f86d7b7026a01b74
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-06-26 14:16:13 -0500

    switching tutorials, check video at 2:35:22 to resume

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
+import { customersData, customersGrid } from '../data/dummy'
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 p-2 md:p10 bg-white rounded-3xl">
+      <Header title="Customers" category="Page" />
+      <GridComponent width="auto" dataSource={customersData} allowPaging allowSorting editSettings={{ allowDeleting: true, allowEditing: true }} toolbar={['Delete']}>
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Edit, Sort, Filter, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Edit, Inject, Toolbar } from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
+
+// dummy data
+import { employeesData, contextMenuItems, employeesGrid } from '../data/dummy'
+
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 p-2 md:p10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent width="auto" dataSource={employeesData} allowPaging allowSorting toolbar={['Search']}>
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
